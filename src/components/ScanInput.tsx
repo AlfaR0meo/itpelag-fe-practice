a715b68e1e81cde3a71e493a8bf15312ee4a6f8e
Alfa Romeo <galtsov-denis-sevsu@mail.ru>
2024-05-06 17:47:17 +0300

    correct picture from video stream ✅

--- src/components/ScanInput.tsx
+++ src/components/ScanInput.tsx
@@ -16,34 +16,42 @@ const readerOptions: ReaderOptions = {
 export default function ScanInput() {
     const videoRef = useRef<HTMLVideoElement>(null);
     const canvasRef = useRef<HTMLCanvasElement>(null);
+    const picturesRef = useRef<HTMLDivElement>(null);
     const [scanResult, setScanResult] = useState<ReadResult[]>();
     const [error, setError] = useState<string>('');
     const [test, setTest] = useState<number>(0);
+    const [info, setInfo] = useState<number>(0);
 
     function detectQRCodeFromVideo() {
-        setInterval(async () => {
-            const video = videoRef.current;
-            const canvas = canvasRef.current;
+        const videoElement = videoRef.current;
+        const canvasElement = canvasRef.current;
+        const picturesElement = picturesRef.current;
 
-            setTest(t => t + 1); //работает
+        if (canvasElement && videoElement && picturesElement) {
+            canvasElement.width = videoElement.offsetWidth;
+            canvasElement.height = videoElement.offsetHeight;
 
-            if (canvas && video) {
-                const w = video.videoWidth;
-                const h = video.videoHeight;
+            const ctx = canvasElement.getContext('2d', {
+                willReadFrequently: true
+            });
 
-                canvas.width = w;
-                canvas.height = h;
+            if (ctx) {
+                setInterval(async () => {
+                    ctx.drawImage(videoElement, 0, 0);
+                    let imgData = canvasElement.toDataURL('image/png');
+                    let imgElement = document.createElement('img');
+                    imgElement.src = imgData;
+                    imgElement.alt = 'Scan picture';
+                    picturesElement.insertAdjacentElement('beforeend', imgElement);
 
-                const canvasCtx = canvas.getContext('2d', {
-                    willReadFrequently: true
-                });
-
-                while (canvasCtx && video) {
-                    canvasCtx.drawImage(video, 0, 0, w, h);
-                    setScanResult(await readBarcodesFromImageData(canvasCtx.getImageData(0, 0, w, h), readerOptions));
-                }
+                    setInfo(Math.round(Math.random() * 100));
+                }, 1000);
             }
-        }, 1000);
+
+            // setTest(t => t + 1);
+            // ctx.drawImage(videoElement, 0, 0);
+            // setScanResult(await readBarcodesFromImageData(canvasCtx.getImageData(0, 0, w, h), readerOptions));
+        }
     }
 
     async function startScan() {
@@ -95,12 +103,13 @@ export default function ScanInput() {
                 <span className='bottom-right'></span>
 
                 <video ref={videoRef}></video>
-                <canvas ref={canvasRef} hidden></canvas>
+                <canvas ref={canvasRef}></canvas>
             </div>
 
             {error && <div className="result result--error">{error}</div>}
-
-            {test && <div>{test}</div>}
+            <div className="result result--info">{info}</div>
+            {/* <div>Тест инкремент переменной: {test}</div> */}
+            <div ref={picturesRef} className="pictures"></div>
 
             {scanResult && (scanResult?.length === 0 ?
                 <Result status={'error'} /> :
